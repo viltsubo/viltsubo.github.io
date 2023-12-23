87e47b5d519ee649b0b14aaa8f5267ba0878581e
viltsubo <majis.ville@gmail.com>
2023-12-23 02:38:08 +0200

    New audio, better slideshow page, bug fixes

--- main.js
+++ main.js
@@ -54,8 +54,8 @@ const factoryCPS = 260
 
 const imgs = ['imgs/1.jpg', 'imgs/2.jpg', 'imgs/3.jpg', 'imgs/4.jpg', 'imgs/5.jpg', 'imgs/6.jpg', 'imgs/7.jpg', 'imgs/8.jpg', 'imgs/9.jpg', 'imgs/10.jpg', 'imgs/11.jpg', 'imgs/12.jpg', 'imgs/13.jpg', 'imgs/14.jpg', 'imgs/15.jpg', 'imgs/16.jpg', 'imgs/17.jpg', 'imgs/18.jpg', 'imgs/19.jpg', 'imgs/20.jpg', 'imgs/21.jpg', 'imgs/22.jpg', 'imgs/23.jpg', 'imgs/24.jpg', 'imgs/25.jpg', 'imgs/26.jpg', 'imgs/27.jpg', 'imgs/28.jpg', 'imgs/29.jpg', 'imgs/30.jpg', 'imgs/31.jpg', 'imgs/32.jpg', 'imgs/33.jpg', 'imgs/34.jpg', 'imgs/35.jpg', 'imgs/36.jpg', 'imgs/37.jpg', 'imgs/38.jpg', 'imgs/39.jpg', 'imgs/40.jpg', 'imgs/41.jpg', 'imgs/42.jpg', 'imgs/43.jpg', 'imgs/44.jpg', 'imgs/45.jpg', 'imgs/46.jpg', 'imgs/47.jpg', 'imgs/48.jpg', 'imgs/49.jpg']
 const imgNumber = 49
-const audios = ['audio/SFX 1.mp3', 'audio/SFX 2.mp3', 'audio/SFX 3 ALL.mp3', 'audio/SFX 3 PART.mp3', 'audio/SFX 4.mp3', 'audio/SFX 5.mp3', 'audio/SFX 6.mp3', 'audio/SFX 7.mp3', 'audio/SFX 8.mp3']
-const audioNumber = 9
+const audios = ['audio/SFX 1.mp3', 'audio/SFX 2.mp3', 'audio/SFX 3 ALL.mp3', 'audio/SFX 3 PART.mp3', 'audio/SFX 4.mp3', 'audio/SFX 5.mp3', 'audio/SFX 6.mp3', 'audio/SFX 7.mp3', 'audio/SFX 8.mp3', 'audio/SFX 9.mp3', 'audio/SFX 10.mp3', 'audio/SFX 11.mp3', 'audio/SFX 12.mp3', 'audio/SFX 13.mp3', 'audio/SFX 14.mp3', 'audio/SFX 15.mp3', 'audio/SFX 16.mp3', 'audio/SFX 17.mp3', 'audio/SFX 18.mp3', 'audio/SFX 19.mp3', 'audio/SFX 20.mp3', 'audio/SFX 21.mp3', 'audio/SFX 22.mp3', 'audio/SFX 23.mp3', 'audio/SFX 24.mp3']
+const audioNumber = 25
 
 
 // Handles when cookie is clicked
--- slideshow.js
+++ slideshow.js
@@ -26,3 +26,41 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+// ChatGPT made this to calculate time between two dates
+function getDateDifference(startDate, endDate) {
+  // Calculate the difference in milliseconds
+  var timeDifference = endDate.getTime() - startDate.getTime();
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  return {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  };
+}
+
+
+let timer = setInterval(function() {
+  let startDate = new Date(); // Current date and time
+  let endDate = new Date('2024-06-24T00:00:00');
+
+  let difference = getDateDifference(startDate, endDate);
+
+  let days = document.getElementById('days')
+  let hours = document.getElementById('hours')
+  let minutes = document.getElementById('minutes')
+  let seconds = document.getElementById('seconds')
+
+  days.innerHTML = '<b>Päiviä: </b>' + difference.days
+  hours.innerHTML = '<b>Tunteja: </b>' + difference.hours
+  minutes.innerHTML = '<b>Minuutteja: </b>' + difference.minutes
+  seconds.innerHTML = '<b>Sekunteja: </b>' + difference.seconds
+}, 1000)
